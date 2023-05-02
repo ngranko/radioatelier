12fea2c0123a19c0ea426d31be973fa6e3376caf
Nikita Granko <igrannick@gmail.com>
2023-05-03 02:08:07 +0400

    Adding a very minimal landing page version

---
+++ web/src/components/Links.svelte
@@ -0,0 +1,31 @@
+<div class="links">
+    <a href="https://instagram.com/radio__atelier/" target="_blank">
+        <img class="igIcon" src="/instagram_icon.svg" alt="instagram" />
+    </a>
+    <a href="https://t.me/radioatelier" target="_blank">
+        <img class="tgIcon" src="/telegram_icon.svg" alt="telegram" />
+    </a>
+</div>
+
+<style lang="scss">
+    .links {
+        display: flex;
+        width: 320px;
+        justify-content: space-between;
+        align-items: flex-start;
+        margin-top: 64px;
+        margin-bottom: 80px;
+    }
+
+    .igIcon {
+        width: 80px;
+        height: 80px;
+        filter: invert(1);
+    }
+
+    .tgIcon {
+        width: 84px;
+        height: 84px;
+        filter: invert(1);
+    }
+</style>
