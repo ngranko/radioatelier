9ef9c2306d1eb523fba24dcfb0421e4a7521708c
Nikita Granko <igrannick@gmail.com>
2023-05-03 02:14:59 +0400

    Adding a very minimal landing page version (#1)

---
+++ web/src/components/Logo.svelte
@@ -0,0 +1,19 @@
+<script>
+    import LogoLight from './LogoLight.svelte';
+    import LogoDark from './LogoDark.svelte';
+    import {defaultHeight, defaultWidth} from '../models/logo.ts';
+</script>
+
+<div class="logo" style="--default-width:{defaultWidth}px;--default-height:{defaultHeight}px;">
+    <LogoDark />
+    <LogoLight />
+</div>
+
+<style lang="scss">
+    .logo {
+        width: min(var(--default-width), 90vw);
+        height: min(var(--default-height), 90vh);
+        margin-top: 80px;
+        position: relative;
+    }
+</style>
