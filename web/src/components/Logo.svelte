05d358fafe497daa7cc16aea5c2cf54a0aacfaeb
Nikita Granko <igrannick@gmail.com>
2023-05-05 01:33:49 +0400

    Proper size calculation for the logo container

--- web/src/components/Logo.svelte
+++ web/src/components/Logo.svelte
@@ -1,19 +1,8 @@
-<script>
-    import LogoLight from './LogoLight.svelte';
-    import LogoDark from './LogoDark.svelte';
-    import {defaultHeight, defaultWidth} from '../models/logo.ts';
-</script>
+<script ✂prettier:content✂="CiAgICBpbXBvcnQgTG9nb0xpZ2h0IGZyb20gJy4vTG9nb0xpZ2h0LnN2ZWx0ZSc7CiAgICBpbXBvcnQgTG9nb0RhcmsgZnJvbSAnLi9Mb2dvRGFyay5zdmVsdGUnOwogICAgaW1wb3J0IHtkZWZhdWx0SGVpZ2h0LCBkZWZhdWx0V2lkdGh9IGZyb20gJy4uL21vZGVscy9sb2dvLnRzJzsK">{}</script>
 
 <div class="logo" style="--default-width:{defaultWidth}px;--default-height:{defaultHeight}px;">
     <LogoDark />
     <LogoLight />
 </div>
 
-<style lang="scss">
-    .logo {
-        width: min(var(--default-width), 90vw);
-        height: min(var(--default-height), 90vh);
-        margin-top: 80px;
-        position: relative;
-    }
-</style>
+<style lang="scss" ✂prettier:content✂="CiAgICAubG9nbyB7CiAgICAgICAgd2lkdGg6IG1pbih2YXIoLS1kZWZhdWx0LXdpZHRoKSwgOTB2dyk7CiAgICAgICAgaGVpZ2h0OiBjYWxjKG1pbih2YXIoLS1kZWZhdWx0LXdpZHRoKSwgOTB2dykgKiAwLjM2NjYpOwogICAgICAgIG1hcmdpbi10b3A6IDgwcHg7CiAgICAgICAgcG9zaXRpb246IHJlbGF0aXZlOwogICAgfQo="></style>
