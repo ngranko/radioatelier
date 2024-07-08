79deb2df910ea55b830993a4f54901e547fddbb6
Nikita Granko <igrannick@gmail.com>
2024-07-08 16:53:53 +0400

    Removing hardcoded host from the login api call

--- web/src/lib/api/auth.ts
+++ web/src/lib/api/auth.ts
@@ -1,11 +1,11 @@
 import {METHOD_POST} from '$lib/api/constants';
-import type {LoginFormInputs, LoginResponsePayload,} from '$lib/interfaces/auth';
+import type {LoginFormInputs, LoginResponsePayload} from '$lib/interfaces/auth';
 import AuthRequest from '$lib/api/request/AuthRequest';
 import type {Payload} from '$lib/interfaces/api';
 import JsonRequest from '$lib/api/request/JsonRequest';
 
 export async function login(values: LoginFormInputs): Promise<LoginResponsePayload> {
-    return new JsonRequest('https://radioatelier.test/api/user/login', METHOD_POST).setParams(values).send();
+    return new JsonRequest('/api/user/login', METHOD_POST).setParams(values).send();
 }
 
 export async function logout(): Promise<Payload> {
