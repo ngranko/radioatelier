ca562028657c1e57a65cc381379085d4588798be
Nikita Granko <igrannick@gmail.com>
2025-04-07 01:17:18 +0400

    Trying to fix the display for the location marker with orientation

--- web/src/lib/components/map/locationMarker.svelte
+++ web/src/lib/components/map/locationMarker.svelte
@@ -46,7 +46,7 @@
 
         const degrees = event.webkitCompassHeading ? event.webkitCompassHeading : event.alpha;
         console.log(degrees);
-        marker.style.transform = `translate(0, 50%) rotate(${degrees}deg)`;
+        marker.style.transform = `rotate(${degrees}deg)`;
     }
 
     function updateCurrentPosition(forceStale = false) {
