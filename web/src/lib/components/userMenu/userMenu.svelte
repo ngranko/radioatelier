e6a18e9d3f79ea793d379bcbe875c8e932e1225a
Nikita Granko <igrannick@gmail.com>
2025-09-20 23:44:21 +0400

    Improving z-index of the user menu

--- web/src/lib/components/userMenu/userMenu.svelte
+++ web/src/lib/components/userMenu/userMenu.svelte
@@ -39,7 +39,7 @@
         cursor: pointer;
         color: colors.$black;
         transition: background-color 0.2s;
-        z-index: 3;
+        z-index: 2;
 
         &:hover {
             background-color: color.scale(colors.$primary, $lightness: +60%);
