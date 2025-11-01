66359d8f5848c202d7f5039c58a5904a54f77489
Nikita Granko <igrannick@gmail.com>
2025-11-01 13:29:11 +0400

    Minor styling change

--- web/src/lib/components/userMenu/userMenu.svelte
+++ web/src/lib/components/userMenu/userMenu.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
     import {Root as AvatarRoot, Fallback} from '$lib/components/ui/avatar';
     import {Button} from '$lib/components/ui/button';
-    import {Root as DropdownMenuRoot, Trigger as DropdownMenuTrigger, Content as DropdownMenuContent, Group, Separator as DropdownMenuSeparator, Item as DropdownMenuItem} from '$lib/components/ui/dropdown-menu';
-    import { toast } from 'svelte-sonner';
+    import {
+        Root as DropdownMenuRoot,
+        Trigger as DropdownMenuTrigger,
+        Content as DropdownMenuContent,
+        Group,
+        Separator as DropdownMenuSeparator,
+        Item as DropdownMenuItem,
+    } from '$lib/components/ui/dropdown-menu';
+    import {toast} from 'svelte-sonner';
     import type {Component} from 'svelte';
     import LogoutDialog from '$lib/components/userMenu/logoutDialog.svelte';
     import {cubicInOut} from 'svelte/easing';
@@ -46,7 +53,7 @@
         <Button
             variant="ghost"
             size="icon"
-            class="relative z-2 m-2 size-10 rounded-full text-3xl"
+            class="relative z-2 m-2 size-10 rounded-full text-3xl shadow-lg"
             aria-label="Показать меню"
         >
             <AvatarRoot class="size-10 rounded-full">
