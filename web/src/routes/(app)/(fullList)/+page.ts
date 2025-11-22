6922f411a7c1ed30c3442f726144662de06171d1
Nikita Granko <igrannick@gmail.com>
2025-11-23 01:26:53 +0400

    Improving returning to last page after a logout

--- web/src/routes/(app)/(fullList)/+page.ts
+++ web/src/routes/(app)/(fullList)/+page.ts
@@ -1,10 +1,10 @@
 import {redirect} from '@sveltejs/kit';
 import type {PageLoad} from './$types';
 
-export const load: PageLoad = async ({parent}) => {
+export const load: PageLoad = async ({parent, url}) => {
     const data = await parent();
     if (!data.user.auth) {
-        redirect(302, '/login');
+        redirect(302, `/login?ref=${encodeURIComponent(url.pathname)}`);
     }
 
     return data;
