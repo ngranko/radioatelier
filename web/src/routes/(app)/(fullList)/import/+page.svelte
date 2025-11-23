dadce50d93e0dc24d955d9318d09697ca93d65da
Nikita Granko <igrannick@gmail.com>
2025-11-24 03:22:05 +0400

    New route for the import functionality

---
+++ web/src/routes/(app)/(fullList)/import/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import ImportDialog from '$lib/components/userMenu/importDialog.svelte';
+
+    let isImportDialogOpen = $state(true);
+</script>
+
+<ImportDialog bind:isOpen={isImportDialogOpen} />
