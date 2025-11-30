918b7b56c4edaf8d0d202de20086a15e81d01ca8
Nikita Granko <igrannick@gmail.com>
2025-11-30 20:16:03 +0300

    Fixing the hydration issues

--- web/src/lib/components/userMenu/loggedInMenu.svelte
+++ web/src/lib/components/userMenu/loggedInMenu.svelte
@@ -9,37 +9,17 @@
         Separator as DropdownMenuSeparator,
         Item as DropdownMenuItem,
     } from '$lib/components/ui/dropdown-menu';
-    import {toast} from 'svelte-sonner';
-    import type {Component} from 'svelte';
     import LogoutDialog from '$lib/components/userMenu/logoutDialog.svelte';
-    import {cubicInOut} from 'svelte/easing';
-    import Loader from '$lib/components/loader.svelte';
-    import {fade} from 'svelte/transition';
     import {goto} from '$app/navigation';
 
-    let isChangePasswordDialogOpen = $state(false);
     let isLogoutDialogOpen = $state(false);
-    let isDialogLoading = $state(false);
-    let PasswordChangeDialog: Component<{isOpen: boolean}> | undefined = $state();
 
     function handleImportClick() {
         goto('/import');
     }
 
-    async function handleChangePasswordClick() {
-        isChangePasswordDialogOpen = true;
-        if (!PasswordChangeDialog) {
-            isDialogLoading = true;
-            try {
-                const {default: Component} = await import(
-                    '$lib/components/userMenu/passwordChangeDialog.svelte'
-                );
-                PasswordChangeDialog = Component;
-            } catch (error) {
-                toast.error('Что-то пошло не так');
-            }
-            isDialogLoading = false;
-        }
+    function handleChangePasswordClick() {
+        goto('/change-password');
     }
 
     function handleLogoutClick() {
@@ -49,20 +29,23 @@
 
 <DropdownMenuRoot>
     <DropdownMenuTrigger>
-        <Button
-            variant="ghost"
-            size="icon"
-            class="relative z-2 m-2 size-10 rounded-full text-3xl shadow-lg"
-            aria-label="Показать меню"
-        >
-            <AvatarRoot class="size-10 rounded-full">
-                <Fallback
-                    class="bg-primary items-end rounded-full pt-1 pr-1 pl-1 brightness-200 saturate-25 transition hover:brightness-180 hover:saturate-30"
-                >
-                    <i class="fa-solid fa-user-ninja"></i>
-                </Fallback>
-            </AvatarRoot>
-        </Button>
+        {#snippet child({ props })}
+            <Button
+                {...props}
+                variant="ghost"
+                size="icon"
+                class="relative z-2 m-2 size-10 rounded-full text-3xl shadow-lg"
+                aria-label="Показать меню"
+            >
+                <AvatarRoot class="size-10 rounded-full">
+                    <Fallback
+                        class="bg-primary items-end rounded-full pt-1 pr-1 pl-1 brightness-200 saturate-25 transition hover:brightness-180 hover:saturate-30"
+                    >
+                        <i class="fa-solid fa-user-ninja"></i>
+                    </Fallback>
+                </AvatarRoot>
+            </Button>
+        {/snippet}
     </DropdownMenuTrigger>
     <DropdownMenuContent class="mr-4">
         <Group>
@@ -82,18 +65,5 @@
         </Group>
     </DropdownMenuContent>
 </DropdownMenuRoot>
-
-{#if isDialogLoading}
-    <div
-        class="fixed inset-0 z-2 flex items-center justify-center bg-black/30"
-        transition:fade={{duration: 100, easing: cubicInOut}}
-    >
-        <Loader />
-    </div>
-{/if}
-
-{#if PasswordChangeDialog}
-    <PasswordChangeDialog bind:isOpen={isChangePasswordDialogOpen} />
-{/if}
 
 <LogoutDialog bind:isOpen={isLogoutDialogOpen} />
--- web/src/lib/components/userMenu/loggedOutMenu.svelte
+++ web/src/lib/components/userMenu/loggedOutMenu.svelte
@@ -18,20 +18,23 @@
 
 <DropdownMenuRoot>
     <DropdownMenuTrigger>
-        <Button
-            variant="ghost"
-            size="icon"
-            class="relative z-2 m-2 size-10 rounded-full text-3xl shadow-lg"
-            aria-label="Показать меню"
-        >
-            <AvatarRoot class="size-10 rounded-full">
-                <Fallback
-                    class="bg-primary items-end rounded-full pt-1 pr-1 pl-1 brightness-200 saturate-25 transition hover:brightness-180 hover:saturate-30"
-                >
-                    <i class="fa-solid fa-user-slash"></i>
-                </Fallback>
-            </AvatarRoot>
-        </Button>
+        {#snippet child({ props })}
+            <Button
+                {...props}
+                variant="ghost"
+                size="icon"
+                class="relative z-2 m-2 size-10 rounded-full text-3xl shadow-lg"
+                aria-label="Показать меню"
+            >
+                <AvatarRoot class="size-10 rounded-full">
+                    <Fallback
+                        class="bg-primary items-end rounded-full pt-1 pr-1 pl-1 brightness-200 saturate-25 transition hover:brightness-180 hover:saturate-30"
+                    >
+                        <i class="fa-solid fa-user-slash"></i>
+                    </Fallback>
+                </AvatarRoot>
+            </Button>
+        {/snippet}
     </DropdownMenuTrigger>
     <DropdownMenuContent class="mr-4">
         <Group>
