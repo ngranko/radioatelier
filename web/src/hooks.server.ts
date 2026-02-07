c611455e2ca137dfa90e8d0bdc9dfe025472a2b6
Nikita Granko <igrannick@gmail.com>
2026-02-07 14:44:42 +0300

    Trying to rewrite urls differently

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -1,13 +1,28 @@
+import type {HandleFetch} from '@sveltejs/kit';
+
 export const handle = async ({event, resolve}) => {
+    return resolve(event);
+};
+
+export const handleFetch: HandleFetch = async ({event, request, fetch}) => {
     const forwardedHost = event.request.headers.get('x-forwarded-host');
     const forwardedProto = event.request.headers.get('x-forwarded-proto');
 
-    if (forwardedHost) {
-        event.url.host = forwardedHost;
-    }
-    if (forwardedProto) {
-        event.url.protocol = `${forwardedProto}:`;
+    if (!forwardedHost && !forwardedProto) {
+        return fetch(request);
     }
 
-    return resolve(event);
+    const url = new URL(request.url);
+    if (url.origin !== event.url.origin) {
+        return fetch(request);
+    }
+
+    if (forwardedHost) {
+        url.host = forwardedHost;
+    }
+    if (forwardedProto) {
+        url.protocol = forwardedProto.endsWith(':') ? forwardedProto : `${forwardedProto}:`;
+    }
+
+    return fetch(new Request(url, request));
 };
