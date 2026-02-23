4ad343bf649ee9e12914a2a8f7319bd4e03d33bb
Nikita Granko <igrannick@gmail.com>
2026-02-24 02:11:40 +0400

    No throw redirect

--- web/src/routes/(app)/(fullList)/+layout.server.ts
+++ web/src/routes/(app)/(fullList)/+layout.server.ts
@@ -6,7 +6,7 @@ export const load = async ({locals}) => {
     const {client, token} = await getConvexClient(locals);
     if (!token) {
         // TODO: should I just return here if I want share pages to work without auth?
-        throw redirect(303, '/login');
+        redirect(303, '/login');
     }
 
     const objects = await client.query(api.markers.list, {});
