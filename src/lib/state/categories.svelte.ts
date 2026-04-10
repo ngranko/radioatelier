2149756bcc3b52cb1fcbbb40e753c88e47e722d7
Nikita Granko <igrannick@gmail.com>
2026-04-10 19:25:44 +0400

    Revert "Another try"
    This reverts commit 82b59f957146adf228d6d1a63f07f2017e7134f0.

--- src/lib/state/categories.svelte.ts
+++ src/lib/state/categories.svelte.ts
@@ -28,25 +28,17 @@ function isMarkerIconKey(value: string): value is MarkerIconKey {
 }
 
 export function setCategories(categories: RawCategory[]) {
-    console.log(categories);
-    const byId = {} as {[id: Id<'categories'>]: Category};
-    const normalizedCategories: Category[] = [];
-
-    for (const item of categories) {
-        if (byId[item.id]) {
-            continue;
-        }
-
-        const normalizedCategory: Category = {
-            ...item,
-            markerColor: isMarkerColor(item.markerColor) ? item.markerColor : MARKER_COLORS[0],
-            markerIcon: isMarkerIconKey(item.markerIcon) ? item.markerIcon : MARKER_ICON_KEYS[0],
-        };
-
-        byId[normalizedCategory.id] = normalizedCategory;
-        normalizedCategories.push(normalizedCategory);
-    }
-
+    const normalizedCategories = categories.map(item => ({
+        ...item,
+        markerColor: isMarkerColor(item.markerColor) ? item.markerColor : MARKER_COLORS[0],
+        markerIcon: isMarkerIconKey(item.markerIcon) ? item.markerIcon : MARKER_ICON_KEYS[0],
+    }));
     categoriesState.list = normalizedCategories;
-    categoriesState.categories = byId;
+    categoriesState.categories = normalizedCategories.reduce(
+        (acc, item) => {
+            acc[item.id] = item;
+            return acc;
+        },
+        {} as {[id: Id<'categories'>]: Category},
+    );
 }
