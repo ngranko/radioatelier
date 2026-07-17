214f8f9b1536a8a4d9bcbbfbecd505e39a237846
Nikita Granko <igrannick@gmail.com>
2026-07-18 01:51:23 +0300

    Trying to enable logs in the posthog integration

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,6 +1,12 @@
-import {PUBLIC_POSTHOG_HOST, PUBLIC_POSTHOG_PROJECT_TOKEN} from '$env/static/public';
+import {dev} from '$app/environment';
+import {
+    PUBLIC_GIT_COMMIT_SHA,
+    PUBLIC_POSTHOG_HOST,
+    PUBLIC_POSTHOG_PROJECT_TOKEN,
+} from '$env/static/public';
 import type {HandleClientError} from '@sveltejs/kit';
 import posthog from 'posthog-js';
+import packageJson from '../package.json';
 
 export async function init() {
     posthog.init(PUBLIC_POSTHOG_PROJECT_TOKEN, {
@@ -8,6 +14,11 @@ export async function init() {
         ui_host: PUBLIC_POSTHOG_HOST,
         defaults: '2026-01-30',
         capture_exceptions: true,
+        logs: {
+            serviceName: 'radioatelier-web',
+            environment: dev ? 'development' : 'production',
+            serviceVersion: `${packageJson.version}+${PUBLIC_GIT_COMMIT_SHA.slice(0, 7)}`,
+        },
     });
 }
 
